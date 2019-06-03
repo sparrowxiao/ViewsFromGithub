da6480e0834b785af10d816895febc962c9beb3a
Ting <workspacext@gmail.com>
2019-06-03 19:52:49 -0400

    Initial Commit

---
+++ ViewsFromGithub/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ViewsFromGithub
+//
+//  Created by Ting Shoemaker on 6/3/19.
+//  Copyright © 2019 Tingswork. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ViewsFromGithub/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ViewsFromGithub
+//
+//  Created by Ting Shoemaker on 6/3/19.
+//  Copyright © 2019 Tingswork. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
