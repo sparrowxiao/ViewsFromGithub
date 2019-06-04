4d644e0467c643f9cc54f6539109e68c9eae40b0
Ting Shoemaker <tshoemaker@easycare.com>
2019-06-04 16:39:52 -0400

    day2 work: REST API

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # ViewsFromGithub
-To create a project to call a RESTful API and use other essential skills for iOS development
+To create a project of calling a RESTful API and using other essential skills for iOS development
 
-- [ ] REST API
+Day1:
+- [x] Made a most regular http connection worked
+I'd like to read proofed the documentation, so Foundation owns the basic swift API to make http request
+https://developer.apple.com/documentation/foundation/url_loading_system/fetching_website_data_into_memory
+
+I named a function getHTMLContent()
+
+Day1:
+- [ ] Github REST API 
+So far the latest Github REST API version is here[https://developer.github.com/v3/]
+Sometimes I  check the traffic of my public repos, as we know, if you want to check the numbers of views from website, you have to click the repo > insights > Traffic, too many clicks. To me, yes if I want to check it everyday.
+
+Here, we can have the easy way to do it 
+https://developer.github.com/v3/repos/traffic/#views
+I'd like to test the command line in Terminal to see the right responsed data format.
+```
+curl -i -H "Authorization: token xxx_your_token_created_in_github" https://api.github.com/repos/sparrowxiao/HTML-Email-Template/traffic/views
+```
+
+
+
+Todo:
+- [ ] Error Handling
+- [ ] Unit Test
--- ViewsFromGithub/FirstViewController.swift
+++ ViewsFromGithub/FirstViewController.swift
@@ -14,34 +14,82 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.startLoad()
+        //1st example to use URLSession to get http request for html content
+        self.startLoadHTML()
+        
+        //2st example to use URLSession to get Response from Github REST API
+        self.getRESTContent()
 
 
 }
 
-func startLoad() {
-    let url = URL(string: "https://tingswork.com")!
-    let task = URLSession.shared.dataTask(with: url) { data, response, error in
-        if let error = error {
-            //self.handleClientError(error)
-            return
-        }
-        guard let httpResponse = response as? HTTPURLResponse,
-            (200...299).contains(httpResponse.statusCode) else {
-                //self.handleServerError(response)
+    func startLoadHTML() {
+        let url = URL(string: "https://tingswork.com")!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                //self.handleClientError(error)
                 return
-        }
-        if let mimeType = httpResponse.mimeType, mimeType == "text/html",
-            let data = data,
-            let string = String(data: data, encoding: .utf8) {
-            DispatchQueue.main.async {
-                //self.webView.loadHTMLString(string, baseURL: url)
-                print(string);
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                (200...299).contains(httpResponse.statusCode) else {
+                    //self.handleServerError(response)
+                    return
+            }
+            if let mimeType = httpResponse.mimeType, mimeType == "text/html",
+                let data = data,
+                let string = String(data: data, encoding: .utf8) {
+                DispatchQueue.main.async {
+                    //self.webView.loadHTMLString(string, baseURL: url)
+                    print(string);
+                }
             }
         }
+        task.resume()
     }
-    task.resume()
-}
+    
+    
+    func getRESTContent(){
+        //prepare the url to request
+        //1. https://api.github.com/users/sparrowxiao
+        //2. put your request of the views of your repo
+        guard let url = URL(string: "https://api.github.com/repos/sparrowxiao/HTML-Email-Template/traffic/views") else {
+            return
+        }
+        
+        let strToken = "input your token :-]."
+        
+        //compose the request
+        var urlRequest = URLRequest(url: url,
+                                    cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                                    timeoutInterval: 10.0 * 1000)
+        urlRequest.httpMethod = "GET"
+        //http header
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        //token info
+        urlRequest.addValue("Token \(strToken)", forHTTPHeaderField: "Authorization")
+            
+        
+        //start to request
+        let task = URLSession.shared.dataTask(with: urlRequest)
+        { (data, response, error) -> Void in
+            guard error == nil else {
+                print("Error while fetching github: \(error)")
+                return
+            }
+            //get the return data(JSON Format)
+            guard let data = data,
+                let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+                    print("Nil data received from fetchAllRooms service")
+                    return
+            }
+            
+            
+        }
+        
+        //done
+        task.resume()
+        
+    }
 
     
 
