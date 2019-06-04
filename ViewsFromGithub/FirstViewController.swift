1643ffac4ff238fe6cb73158b34ff2f6610c54e0
Ting <workspacext@gmail.com>
2019-06-03 22:33:08 -0400

    To create a project to call a RESTful API  and use other essential skills for developing iOS

--- ViewsFromGithub/FirstViewController.swift
+++ ViewsFromGithub/FirstViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import Foundation
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        self.startLoad()
 
 
 }
 
+func startLoad() {
+    let url = URL(string: "https://tingswork.com")!
+    let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        if let error = error {
+            //self.handleClientError(error)
+            return
+        }
+        guard let httpResponse = response as? HTTPURLResponse,
+            (200...299).contains(httpResponse.statusCode) else {
+                //self.handleServerError(response)
+                return
+        }
+        if let mimeType = httpResponse.mimeType, mimeType == "text/html",
+            let data = data,
+            let string = String(data: data, encoding: .utf8) {
+            DispatchQueue.main.async {
+                //self.webView.loadHTMLString(string, baseURL: url)
+                print(string);
+            }
+        }
+    }
+    task.resume()
+}
+
+    
+
+}
